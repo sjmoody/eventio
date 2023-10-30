af35fbea10e288e993ffe7e49a2c5171ce6cd85e
Steven Moody <smoody07@gmail.com>
2023-10-29 18:02:34 -0700

    clean up BLitz homepage, add about page

---
+++ src/pages/about.tsx
@@ -0,0 +1,8 @@
+import { BlitzPage } from "@blitzjs/next"
+import React from "react"
+
+const AboutPage: BlitzPage = () => {
+  return <div>This is the about page</div>
+}
+
+export default AboutPage
