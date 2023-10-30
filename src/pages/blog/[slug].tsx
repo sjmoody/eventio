fd70689db2470f4d21e2ae6310463550083cb6e6
Steven Moody <smoody07@gmail.com>
2023-10-29 19:27:46 -0700

    refactoring based on the anal preferences of kitze

--- src/pages/blog/[slug].tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import { useStringParam } from "src/utils/utils"
-
-const BlogPostPage = () => {
-  const slug = useStringParam("slug")
-  return <div>Welcome to the blog post: {slug}</div>
-}
-
-export default BlogPostPage
--- src/pages/blog/index.tsx
+++
@@ -1,8 +0,0 @@
-import { BlitzPage } from "@blitzjs/next"
-import React from "react"
-
-const BlogPage = () => {
-  return <div>Welcome to the Blog</div>
-}
-
-export default BlogPage
