d68c734390e73208bd65a976f254aabdaffb069c
Steven Moody <smoody07@gmail.com>
2023-10-31 17:17:52 -0700

    basic authentication form when user is logged out. Uses Mantine form and social buttons

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -2,11 +2,20 @@ import Layout from "src/core/layouts/Layout"
 import { BlitzPage } from "@blitzjs/next"
 import { UserInfo } from "../core/components/UserInfo"
 import { Horizontal, Vertical } from "mantine-layout-components"
+import { AuthenticationForm } from "src/core/components/MainAuthenticationForm"
+import { useCurrentUser } from "src/features/users/hooks/useCurrentUser"
 
 const Home: BlitzPage = () => {
+  const currentUser = useCurrentUser()
+
   return (
     <Layout title="Home">
-      <UserInfo />
+      {currentUser && <UserInfo />}
+      {!currentUser && (
+        <Vertical fullH fullW center>
+          <AuthenticationForm />
+        </Vertical>
+      )}
     </Layout>
   )
 }
